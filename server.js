8c2d1b262e6219c3d5d7123552a6343b489bcc97
singhadarsh0403 <singhadarsh0403@gmail.com>
2019-02-09 17:45:22 +0530

    add project page

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname+ '/views/partials');
@@ -48,6 +49,12 @@ app.get('/about',(req,res)=>{
   });
 });
 
+app.get('/projects',(req,res)=>{
+  res.render('projects.hbs',{
+    pageTitle: 'Projects'
+  });
+});
+
 // /bad -send back json with errorMessage
 
 app.get('/bad',(req,res)=>{
@@ -56,6 +63,6 @@ app.get('/bad',(req,res)=>{
   });
 });
 
-app.listen(3000,()=>{
-  console.log('Server is up on port 3000');
+app.listen(port ,()=>{
+  console.log(`Server is up on port ${port}`);
 });
